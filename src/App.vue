ca93cedf935586f734ca1688e9b1b59f2014f488
tzuchiyu <108826611+tzuchiyu@users.noreply.github.com>
2023-09-24 02:12:50 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import { RouterView } from 'vue-router';
+    import { useCounterStore } from "@/stores/counter.ts" //引入stores
+    useCounterStore(); //使用
+</script>
+
+<template>
+    <RouterView />
+</template>
